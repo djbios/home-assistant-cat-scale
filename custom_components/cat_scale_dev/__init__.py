13d9f7a63110ff6cc8e4f4bdec3b145058937bd5
Lucas Fijen <github@lucasfijen.com>
2025-07-06 11:05:29 +0200

    Disable other config

--- custom_components/cat_scale_dev/__init__.py
+++ custom_components/cat_scale_dev/__init__.py
@@ -25,14 +25,14 @@ async def async_unload_entry(hass: HomeAssistant, entry) -> bool:
     """Unload an entry."""
     return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
 
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    """Set up the integration from config entry."""
+# async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+#     """Set up the integration from config entry."""
     
-    cat_weight_threshold = config_entry.options.get(CONF_CAT_WEIGHT_THRESHOLD, config_entry.data.get(CONF_CAT_WEIGHT_THRESHOLD, DEFAULT_CAT_WEIGHT_THRESHOLD))
-    min_presence_time = config_entry.options.get(CONF_MIN_PRESENCE_TIME, config_entry.data.get(CONF_MIN_PRESENCE_TIME, DEFAULT_MIN_PRESENCE_TIME))
-    leave_timeout = config_entry.options.get(CONF_LEAVE_TIMEOUT, config_entry.data.get(CONF_LEAVE_TIMEOUT, DEFAULT_LEAVE_TIMEOUT))
+#     cat_weight_threshold = config_entry.options.get(CONF_CAT_WEIGHT_THRESHOLD, config_entry.data.get(CONF_CAT_WEIGHT_THRESHOLD, DEFAULT_CAT_WEIGHT_THRESHOLD))
+#     min_presence_time = config_entry.options.get(CONF_MIN_PRESENCE_TIME, config_entry.data.get(CONF_MIN_PRESENCE_TIME, DEFAULT_MIN_PRESENCE_TIME))
+#     leave_timeout = config_entry.options.get(CONF_LEAVE_TIMEOUT, config_entry.data.get(CONF_LEAVE_TIMEOUT, DEFAULT_LEAVE_TIMEOUT))
 
 
-    # Simple pass-through: no domain-level setup needed in this example
-    return True
+#     # Simple pass-through: no domain-level setup needed in this example
+#     return True
 
